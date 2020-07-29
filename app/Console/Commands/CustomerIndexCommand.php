7fc013eb0f28b882bd571841fb5131b75fe42636
matteodeliperi <matteo.deliperi@smart-contact.it>
2020-07-30 00:54:35 +0200

    Dev-ed command customers:get, test issue

--- app/Console/Commands/CustomerIndexCommand.php
+++ app/Console/Commands/CustomerIndexCommand.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use App\Customer;
 use Illuminate\Console\Command;
 
-class CustomerIndex extends Command
+class CustomerIndexCommand extends Command
 {
     /**
      * The name and signature of the console command.
--- tests/Feature/CommandCustomerIndexTest.php
+++ tests/Feature/CommandCustomerIndexTest.php
@@ -12,16 +12,20 @@ class CommandCustomerIndexTest extends TestCase
      */
     public function it_has_customers_index_command()
     {
-       $this->assertTrue(class_exists(\App\Console\Commands\CustomerIndex::class));
+       $this->assertTrue(class_exists(\App\Console\Commands\CustomerIndexCommand::class));
     }
 
+    /**
+     * @test
+     */
     public function guest_can_view_index_customers()
     {
-        $customers = Customer::all();
+        factory(Customer::class)->create();
 
         $this->artisan('customers:get')
             ->expectsOutput('++++++++++++++++++++++++++++++++++++++++++++++++ Stampa Di Tutti i Customers ++++++++++++++++++++++++++++++++++++++++')
-            ->expectsOutput($customers)
-            ->expectsOutput('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+            ->expectsOutput(Customer::all())
+            ->expectsOutput('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+            ->assertExitCode(0);
     }
 }

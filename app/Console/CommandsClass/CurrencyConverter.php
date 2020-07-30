e195309b9b44edc079e7f06cb0b6aafc132fa325
matteodeliperi <matteo.deliperi@smart-contact.it>
2020-07-30 17:07:00 +0200

    Report work!

--- app/Console/CommandsClass/CurrencyConverter.php
+++ app/Console/CommandsClass/CurrencyConverter.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\CommandsClass;
 
+use App\CurrencyWebServer;
 use App\CustomerTransaction;
 
 class CurrencyConverter
@@ -12,6 +13,44 @@ class CurrencyConverter
      */
     public static function main($customerId = null)
     {
-        return CustomerTransaction::whereCustomerId($customerId)->get();
+        $customerTransactions = CustomerTransaction::whereCustomerId($customerId)->get();
+
+        $customerTransactions = self::converter('€', $customerTransactions);
+
+        return $customerTransactions;
+    }
+
+    /**
+     * @param $primaryCurrency
+     * @param $customerTransactions
+     * @return array
+     */
+    public static function converter($primaryCurrency, $customerTransactions)
+    {
+        $currencyIndexChanges = CurrencyWebServer::all()->toArray();
+        $output = [];
+
+        foreach($customerTransactions as $customerTransaction){
+
+            foreach($currencyIndexChanges as $currencyIndexChange){
+
+                if(array_search($customerTransaction->currency, $currencyIndexChange)){
+                    $customerCurrencyIndex = $currencyIndexChange['value'];
+                    break;
+                }
+            }
+
+            $customerTransaction->value =
+
+            (1 / $customerCurrencyIndex) * $customerTransaction->value
+            ;
+
+            $customerTransaction->currency = $primaryCurrency;
+
+            array_push($output, $customerTransaction);
+
+        }
+
+        return $output;
     }
 }
--- database/factories/CustomerTransactionFactory.php
+++ database/factories/CustomerTransactionFactory.php
@@ -9,13 +9,13 @@ use Illuminate\Support\Arr;
 
 /**
  * Il 'customer_id' viene assegnato con valore randomico preso dai reali Id Customer presenti nel db,
- * in caso non ce ne fossero gli viene assegnsto un Customer appena creato
+ * in caso non ce ne fossero gli viene assegnato un Customer appena creato
  */
 $factory->define(CustomerTransaction::class, function (Faker $faker) {
     return [
         'customer_id' => Arr::random(Customer::all()->pluck('id')->toArray()) ??
             factory(Customer::class)->create(),
         'value' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 5000),
-        'currency' => $faker->randomElement($array = array ('£','$','€'))
+        'currency' => $faker->randomElement($array = array ('£','$',mb_convert_encoding('€', 'UTF-8')))
     ];
 });
